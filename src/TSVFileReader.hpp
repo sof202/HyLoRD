2dbebfa9e30e09d180ba26b5772813bd276e783b
sof202 <s.o.fletcher@exeter.ac.uk>
2025-04-07 12:51:12 +0100

    feat: add memory mapping setup and teardown

--- src/TSVFileReader.hpp
+++ src/TSVFileReader.hpp
@@ -1,10 +1,16 @@
 #ifndef TSVFILEREADER_H_
 #define TSVFILEREADER_H_
 
+#include <fcntl.h>
+#include <sys/mman.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
+#include <cerrno>
 #include <functional>
+#include <stdexcept>
 #include <string>
+#include <system_error>
 #include <thread>
 #include <vector>
 
@@ -19,7 +25,7 @@ class TSVFileReader {
 
    // Memory mapping
    struct stat m_file_info{};
-   int m_file_size{};
+   std::size_t m_file_size{};
    int m_file_descriptor{-1};
    char* m_mapped_data{nullptr};
    void setupMemoryMap();
@@ -55,5 +61,57 @@ class TSVFileReader {
 
    ~TSVFileReader() { cleanupMemoryMap(); }
 };
+
+template <typename RecordType>
+inline void TSVFileReader<RecordType>::setupMemoryMap() {
+   m_file_descriptor = open(m_file_path.c_str(), O_RDONLY);
+   if (m_file_descriptor == -1) {
+      throw std::system_error(errno,
+                              std::system_category(),
+                              "Failed to open file: " + m_file_path);
+   }
+   if (fstat(m_file_descriptor, &m_file_info) == -1) {
+      int fstat_error_number = errno;
+      close(m_file_descriptor);
+      throw std::system_error(fstat_error_number,
+                              std::system_category(),
+                              "fstat failed for file: " + m_file_path);
+   }
+   if (!S_ISREG(m_file_info.st_mode)) {
+      close(m_file_descriptor);
+      throw std::runtime_error("Not a regular file: " + m_file_path);
+   }
+
+   m_file_size = static_cast<std::size_t>(m_file_info.st_size);
+   if (m_file_size == 0) {
+      close(m_file_descriptor);
+      throw std::runtime_error("This file is empty: " + m_file_path);
+   }
+
+   m_mapped_data = static_cast<char*>(mmap(
+       nullptr, m_file_size, PROT_READ, MAP_PRIVATE, m_file_descriptor, 0));
+
+   if (m_mapped_data == MAP_FAILED) {
+      int mmap_error_number = errno;
+      close(m_file_descriptor);
+      throw std::system_error(mmap_error_number,
+                              std::system_category(),
+                              "Memory mapping failed for: " + m_file_path);
+   }
+
+   madvise(m_mapped_data, m_file_size, MADV_SEQUENTIAL | MADV_WILLNEED);
+}
+
+template <typename RecordType>
+inline void TSVFileReader<RecordType>::cleanupMemoryMap() {
+   if (m_mapped_data) {
+      munmap(m_mapped_data, m_file_size);
+      m_mapped_data = nullptr;
+   }
+   if (m_file_descriptor != -1) {
+      close(m_file_descriptor);
+      m_file_descriptor = -1;
+   }
+}
 
 #endif
