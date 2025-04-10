30a0f909e4530da1547451615ccfa8a7a2138388
sof202 <s.o.fletcher@exeter.ac.uk>
2025-04-10 12:13:24 +0100

    feat: add declaration for a function to find overlapping indexes

--- src/hylord.hpp
+++ src/hylord.hpp
@@ -8,8 +8,15 @@
  */
 
 #include <string_view>
+#include <vector>
 
 namespace Hylord {
+using RowIndexVector = std::vector<std::size_t>;
+
+template <typename BedTypeOne, typename BedTypeTwo>
+std::pair<RowIndexVector, RowIndexVector> findOverLappingIndexes(
+    const BedTypeOne& bed_one, const BedTypeTwo& bed_two);
+
 void run(const std::string_view bedmethyl_file,
          const std::string_view reference_matrix_file,
          const std::string_view cpg_list_file,
