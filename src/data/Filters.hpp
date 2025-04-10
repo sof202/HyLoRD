7ec06b990af64fb5f5e78abeda6ded6947d7ccd5
sof202 <s.o.fletcher@exeter.ac.uk>
2025-04-10 16:13:55 +0100

    feat: add namespace for certain row filters to use when reading in files

---
+++ src/data/Filters.hpp
@@ -0,0 +1,53 @@
+#ifndef FILTERS_H_
+#define FILTERS_H_
+
+#include <stdexcept>
+#include <vector>
+
+#include "types.hpp"
+
+namespace Hylord::Filters {
+using RowFilter = Hylord::IO::RowFilter;
+
+class FilterCombiner {
+  public:
+   void addFilter(RowFilter filter) { m_filters.push_back(filter); }
+   bool operator()(const Fields& row) const {
+      for (const auto& filter : m_filters) {
+         if (!filter(row)) return false;
+      }
+      return true;
+   }
+
+  private:
+   std::vector<RowFilter> m_filters;
+};
+
+template <int min_reads>
+bool has_high_read_count(const Fields& fields) {
+   if (fields.size() < 5) {
+      throw std::out_of_range(
+          "Could not apply row filter, not enough fields.");
+   }
+   return std::stoi(fields[4]) > min_reads;
+}
+
+inline bool is_hydroxy_read(const Fields& fields) {
+   if (fields.size() < 4) {
+      throw std::out_of_range(
+          "Could not apply row filter, not enough fields.");
+   }
+   return fields[3][0] == 'h';
+}
+
+inline bool is_methyl_read(const Fields& fields) {
+   if (fields.size() < 4) {
+      throw std::out_of_range(
+          "Could not apply row filter, not enough fields.");
+   }
+   return fields[3][0] == 'm';
+}
+
+}  // namespace Hylord::Filters
+
+#endif
