31b61aeb3064d87241b39024d8ee14d1f2c731c3
sof202 <s.o.fletcher@exeter.ac.uk>
2025-04-10 11:57:05 +0100

    feat: implement all bed container classes
    feat: add row subsetter

--- src/BedData.hpp
+++ src/BedData.hpp
@@ -2,20 +2,64 @@
 #define BEDDATA_H_
 
 #include <functional>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
+#include "BedRecords.hpp"
+
 using RowFilterFunction = std::function<bool(const std::vector<std::string>&)>;
 using ColumnIndexes = std::vector<std::size_t>;
+using RowIndexes = std::vector<std::size_t>;
 
+namespace Hylord {
 template <typename RecordType>
-class BedData {
+void subsetRows(std::vector<RecordType>& records, const RowIndexes& rows) {
+   std::vector<RecordType> subset_records;
+   subset_records.reserve(rows.size());
+
+   for (auto i : rows) {
+      if (i >= records.size()) throw std::out_of_range("Invalid row index.");
+      subset_records.push_back(std::move(records[i]));
+   }
+   records = std::move(subset_records);
+}
+}  // namespace Hylord
+
+class CpGData {
   public:
-   BedData(std::vector<RecordType> records) : m_records{std::move(records)} {}
-   const std::vector<RecordType>& records() const { return m_records; }
+   CpGData(std::vector<BedRecords::Bed4> records) :
+       m_records{std::move(records)} {}
+
+   const std::vector<BedRecords::Bed4>& records() { return m_records; }
+   void subsetRows(RowIndexes rows) { Hylord::subsetRows(m_records, rows); };
 
   private:
-   std::vector<RecordType> m_records{};
+   std::vector<BedRecords::Bed4> m_records{};
+};
+
+class ReferenceMatrixData {
+  public:
+   ReferenceMatrixData(std::vector<BedRecords::Bed4PlusX> records) :
+       m_records{std::move(records)} {}
+
+   const std::vector<BedRecords::Bed4PlusX>& records() { return m_records; }
+   void subsetRows(RowIndexes rows) { Hylord::subsetRows(m_records, rows); };
+
+  private:
+   std::vector<BedRecords::Bed4PlusX> m_records{};
+};
+
+class BedMethylData {
+  public:
+   BedMethylData(std::vector<BedRecords::Bed9Plus9> records) :
+       m_records{std::move(records)} {}
+
+   const std::vector<BedRecords::Bed9Plus9>& records() { return m_records; }
+   void subsetRows(RowIndexes rows) { Hylord::subsetRows(m_records, rows); };
+
+  private:
+   std::vector<BedRecords::Bed9Plus9> m_records{};
 };
 
 #endif
