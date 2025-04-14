0ec61aa80566b2269a18f6b01de058a5910fb118
sof202 <s.o.fletcher@exeter.ac.uk>
2025-04-14 16:24:19 +0100

    refactor: move index overlap functions next to containers they act on
    This makes more sense to me right now

--- src/data/BedData.hpp
+++ src/data/BedData.hpp
@@ -1,7 +1,9 @@
 #ifndef BEDDATA_H_
 #define BEDDATA_H_
 
+#include <numeric>
 #include <stdexcept>
+#include <tuple>
 #include <vector>
 
 #include "Eigen/Dense"
@@ -65,6 +67,74 @@ class BedMethylData {
   private:
    std::vector<BedRecords::Bed9Plus9> m_records{};
 };
+
+template <typename BedTypeOne, typename BedTypeTwo>
+std::pair<RowIndexes, RowIndexes> findOverLappingIndexes(
+    const BedTypeOne& bed_one, const BedTypeTwo& bed_two) {
+   RowIndexes bed_one_overlapping_indexes{};
+   RowIndexes bed_two_overlapping_indexes{};
+
+   std::size_t min_indexes_to_keep{std::min(bed_one.size(), bed_two.size())};
+   bed_one_overlapping_indexes.reserve(min_indexes_to_keep);
+   bed_two_overlapping_indexes.reserve(min_indexes_to_keep);
+
+   RowIndex bed_one_row{};
+   RowIndex bed_two_row{};
+
+   // We expect the majority of rows to match up already and bed files are
+   // expected to be sorted (modkit will do this). So to find the indexes where
+   // the two bed files overlap, we take a two pointer approach.
+   while (bed_one_row < bed_one.size() && bed_two_row < bed_two.size()) {
+      auto bed_one_key{std::tie(bed_one[bed_one_row].chromosome,
+                                bed_one[bed_one_row].start,
+                                bed_one[bed_one_row].name)};
+      auto bed_two_key{std::tie(bed_two[bed_two_row].chromosome,
+                                bed_two[bed_two_row].start,
+                                bed_two[bed_two_row].name)};
+
+      if (bed_one_key == bed_two_key) {
+         bed_one_overlapping_indexes.push_back(bed_one_row);
+         bed_two_overlapping_indexes.push_back(bed_two_row);
+         bed_one_row++;
+         bed_two_row++;
+      } else if (bed_one_key < bed_two_key) {
+         bed_one_row++;
+      } else {
+         bed_two_row++;
+      }
+   }
+   return {bed_one_overlapping_indexes, bed_two_overlapping_indexes};
+}
+
+template <typename Records>
+RowIndexes findIndexesInCpGList(const BedData::CpGData& cpg_list,
+                                const Records& bed_entries) {
+   const std::vector<BedRecords::Bed4>& cpgs{cpg_list.records()};
+   RowIndexes bed_indexes_in_cpg_list{};
+   bed_indexes_in_cpg_list.reserve(cpgs.size());
+
+   for (RowIndex cpg{}; cpg < cpgs.size(); ++cpg) {
+      auto cpg_key{
+          std::tie(cpgs[cpg].chromosome, cpgs[cpg].start, cpgs[cpg].name)};
+      RowIndex low{};
+      RowIndex high{bed_entries.size() - 1};
+      while (low <= high) {
+         RowIndex mid{std::midpoint(low, high)};
+         auto row_key{std::tie(bed_entries[mid].chromosome,
+                               bed_entries[mid].start,
+                               bed_entries[mid].name)};
+         if (row_key == cpg_key) {
+            bed_indexes_in_cpg_list.push_back(mid);
+            break;
+         } else if (row_key < cpg_key) {
+            low = mid + 1;
+         } else {
+            high = mid - 1;
+         }
+      }
+   }
+   return bed_indexes_in_cpg_list;
+}
 }  // namespace Hylord::BedData
 
 #endif
