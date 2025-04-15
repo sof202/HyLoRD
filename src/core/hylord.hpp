82d8391c85024e68f0708ed1c40fe04e7c5ec8e2
sof202 <s.o.fletcher@exeter.ac.uk>
2025-04-15 10:03:57 +0100

    feat: add wrapper function to read files

--- src/core/hylord.hpp
+++ src/core/hylord.hpp
@@ -9,8 +9,25 @@
 
 #include <string_view>
 
+#include "io/TSVFileReader.hpp"
+#include "types.hpp"
+
 namespace Hylord {
 
+template <typename BedFile, typename BedType>
+BedFile readFile(const std::string_view file_name,
+                 int threads,
+                 const IO::ColumnIndexes& fields_to_extract = {},
+                 IO::RowFilter rowFilter = nullptr) {
+   if (file_name.empty()) return BedFile{};
+
+   return BedFile{[&]() {
+      IO::TSVFileReader<BedType> reader{
+          file_name, fields_to_extract, rowFilter, threads};
+      reader.load();
+      return reader.extractRecords();
+   }()};
+}
 void run(const std::string_view bedmethyl_file,
          const std::string_view reference_matrix_file,
          const std::string_view cpg_list_file,
