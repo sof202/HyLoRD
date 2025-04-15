cd3e800bc7f169d490d29ebcc8bffcdd79c9c805
sof202 <s.o.fletcher@exeter.ac.uk>
2025-04-15 11:12:06 +0100

    feat: add preprocessing functionality

--- src/core/hylord.cpp
+++ src/core/hylord.cpp
@@ -5,9 +5,48 @@
 #include <stdexcept>
 #include <string_view>
 #include <utility>
+
+#include "Eigen/Dense"
+#include "data/BedData.hpp"
+#include "data/BedRecords.hpp"
 #include "types.hpp"
 
 namespace Hylord {
+void preprocessInputData(BedData::BedMethylData& bedmethyl,
+                         BedData::ReferenceMatrixData& reference_matrix,
+                         const BedData::CpGData& cpg_list,
+                         int additional_cell_types) {
+   if (reference_matrix.empty()) {
+      if (additional_cell_types < 1) {
+         throw std::invalid_argument(
+             "If no reference matrix is provided, additional_cell_types "
+             "should "
+             "be set (>0).");
+      }
+      reference_matrix = BedData::ReferenceMatrixData{bedmethyl};
+   }
+   if (!cpg_list.empty()) {
+      reference_matrix.subsetRows(
+          BedData::findIndexesInCpGList(cpg_list, reference_matrix.records()));
+      bedmethyl.subsetRows(
+          BedData::findIndexesInCpGList(cpg_list, bedmethyl.records()));
+   }
+   std::pair<RowIndexes, RowIndexes> overlapping_indexes{
+       BedData::findOverLappingIndexes(reference_matrix.records(),
+                                       bedmethyl.records())};
+
+   if (overlapping_indexes.first.empty() ||
+       overlapping_indexes.second.empty()) {
+      throw std::runtime_error(
+          "No overlapping indexes found between reference matrix and input "
+          "bedmethyl file.");
+   }
+   reference_matrix.subsetRows(overlapping_indexes.first);
+   bedmethyl.subsetRows(overlapping_indexes.second);
+
+   reference_matrix.addMoreCellTypes(additional_cell_types);
+}
+
 int run(const std::string_view bedmethyl_file,
         const std::string_view reference_matrix_file,
         const std::string_view cpg_list_file,
