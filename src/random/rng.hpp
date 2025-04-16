3c7fd398a391349f0ef1fc774a5e93f7977cd7d2
sof202 <s.o.fletcher@exeter.ac.uk>
2025-04-16 12:06:16 +0100

    feat: add proper cdf for hydroxymethylation
    Taken from sox10 data. Would be nice for more amalgamated dataset in the
    future.

--- src/random/rng.hpp
+++ src/random/rng.hpp
@@ -34,16 +34,16 @@ static inline std::vector<double> methylation_cdf{0.06884382,
 // Hydoxymethylation is similar, but shows a massively different distrubtion
 // in neurons than other cell types. We stick with the distributions seen in
 // other cell types seen.
-static inline std::vector<double> hydroxymethylation_cdf{0.06884382,
-                                                         0.10354818,
-                                                         0.12962329,
-                                                         0.16059704,
-                                                         0.20894288,
-                                                         0.27983389,
-                                                         0.38286741,
-                                                         0.53027698,
-                                                         0.76769743,
-                                                         0.97110349,
+static inline std::vector<double> hydroxymethylation_cdf{0.23067502,
+                                                         0.57876935,
+                                                         0.79139396,
+                                                         0.90436016,
+                                                         0.96756705,
+                                                         0.99265250,
+                                                         0.99879729,
+                                                         0.99962567,
+                                                         0.99974549,
+                                                         0.99975449,
                                                          1};
 inline double get_random_methylation() {
    double r{std::uniform_real_distribution<double>(0.0, 1.0)(rng)};
