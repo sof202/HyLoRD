2a0ecaed3f2eedaa65332a423d34a3c4eb74ef65
sof202 <s.o.fletcher@exeter.ac.uk>
2025-05-15 16:28:50 +0100

    test(Bed4): Add bed4 parsing tests

--- test/unit/RecordParsingTest.cpp
+++ test/unit/RecordParsingTest.cpp
@@ -1,10 +1,10 @@
 #include <gtest/gtest.h>
 
 #include <stdexcept>
-#include <string>
 #include <string_view>
 
 #include "data/BedRecords.hpp"
+#include "types.hpp"
 
 namespace Hylord {
 using namespace std::string_view_literals;
@@ -22,4 +22,31 @@ TEST(ChromosomeParsingTest, ThrowsOnInvalidChromosomeName) {
    EXPECT_THROW(BedRecords::parseChromosomeNumber("chrt"), std::runtime_error);
 }
 
+TEST(Bed4ParsingTest, BasicFunctionality) {
+   constexpr BedRecords::Bed4 expected_parsed_fields{1, 1000, 'h'};
+   const Fields input_fields{"chr1", "1000", "1001", "h"};
+   BedRecords::Bed4 actual_parsed_fields{
+       BedRecords::Bed4::fromFields(input_fields)};
+   EXPECT_EQ(actual_parsed_fields.name, expected_parsed_fields.name);
+   EXPECT_EQ(actual_parsed_fields.start, expected_parsed_fields.start);
+   EXPECT_EQ(actual_parsed_fields.name, expected_parsed_fields.name);
+}
+
+TEST(Bed4ParsingTest, HandlesFullSignalNames) {
+   constexpr BedRecords::Bed4 expected_parsed_fields{1, 1000, 'h'};
+   const Fields input_fields{"chr1", "1000", "1001", "hydroxymethylation"};
+   BedRecords::Bed4 actual_parsed_fields{
+       BedRecords::Bed4::fromFields(input_fields)};
+   EXPECT_EQ(actual_parsed_fields.name, expected_parsed_fields.name);
+}
+
+TEST(Bed4ParsingTest, ThrowsOnTooFewFields) {
+   const Fields input_fields{"chr1", "1000"};
+   EXPECT_THROW(BedRecords::Bed4::fromFields(input_fields), std::out_of_range);
+}
+
+TEST(Bed4ParsingTest, ThrowsOnIncorrectFields) {
+   const Fields input_fields{"chr1", "not a number", "121", "h"};
+   EXPECT_THROW(BedRecords::Bed4::fromFields(input_fields), std::exception);
+}
 }  // namespace Hylord
