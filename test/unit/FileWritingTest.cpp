a7271cb6c43a856f8e32072d0610044e744911a7
sof202 <s.o.fletcher@exeter.ac.uk>
2025-05-16 14:08:33 +0100

    refactor(test): Make gtest wrappers to reduce repetition

--- test/unit/FileWritingTest.cpp
+++ test/unit/FileWritingTest.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <iterator>
 #include <sstream>
+#include <string>
 
 #include "HylordException.hpp"
 #include "io/writeMetrics.hpp"
@@ -11,80 +12,58 @@
 namespace Hylord {
 
 class FileWritingTest : public ::testing::Test {
-  public:
+  protected:
    std::filesystem::path m_test_dir;
    std::stringstream m_test_buffer;
-
-  protected:
+   std::string m_test_string{"test string"};
    void SetUp() override {
       m_test_dir = std::filesystem::temp_directory_path() / "test";
       std::filesystem::create_directories(m_test_dir);
    }
    void TearDown() override { std::filesystem::remove_all(m_test_dir); }
+
+   static void writeWrapper(const std::filesystem::path& file_path,
+                            const std::string& input_text) {
+      std::stringstream buffer;
+      buffer << input_text;
+
+      IO::writeToFile(buffer, file_path);
+   }
+   static void testSuccessfulWrite(const std::filesystem::path& file_path,
+                                   const std::string& input_text) {
+      ASSERT_TRUE(std::filesystem::exists(file_path));
+
+      std::ifstream file(file_path);
+      std::string contents((std::istreambuf_iterator<char>(file)),
+                           std::istreambuf_iterator<char>());
+      EXPECT_EQ(contents, input_text);
+   }
 };
 
 TEST_F(FileWritingTest, BasicFunctionality) {
-   m_test_buffer << "test string";
-   std::filesystem::path file_path{m_test_dir / "basic_writing.txt"};
-   IO::writeToFile(m_test_buffer, file_path);
-
-   ASSERT_TRUE(std::filesystem::exists(file_path));
-
-   std::ifstream test_file(file_path);
-   std::string test_file_contents((std::istreambuf_iterator<char>(test_file)),
-                                  std::istreambuf_iterator<char>());
-   EXPECT_EQ(test_file_contents, "test string");
+   writeWrapper(m_test_dir / "basic_writing.txt", m_test_string);
+   testSuccessfulWrite(m_test_dir / "basic_writing.txt", m_test_string);
 }
 
 TEST_F(FileWritingTest, CreatesParentDirectories) {
-   m_test_buffer << "test string";
-   std::filesystem::path nested_file_path{m_test_dir / "subdir1" / "subdir2" /
-                                          "nested_file.txt"};
-   IO::writeToFile(m_test_buffer, nested_file_path);
-
-   ASSERT_TRUE(std::filesystem::exists(nested_file_path));
-
-   std::ifstream test_file(nested_file_path);
-   std::string test_file_contents((std::istreambuf_iterator<char>(test_file)),
-                                  std::istreambuf_iterator<char>());
-   EXPECT_EQ(test_file_contents, "test string");
+   writeWrapper(m_test_dir / "subdir1" / "subdir2" / "basic_writing.txt",
+                m_test_string);
+   testSuccessfulWrite(
+       m_test_dir / "subdir1" / "subdir2" / "basic_writing.txt",
+       m_test_string);
 }
 
 TEST_F(FileWritingTest, HandleExistingFileNames) {
-   std::filesystem::path file_path{m_test_dir / "existing_file.txt"};
-   m_test_buffer << "test string";
-   IO::writeToFile(m_test_buffer, file_path);
-   m_test_buffer << "test string";
-   IO::writeToFile(m_test_buffer, file_path);
-   m_test_buffer << "test string";
-   IO::writeToFile(m_test_buffer, file_path);
-
-   std::filesystem::path file_path_copy1{m_test_dir / "existing_file_1.txt"};
-   std::filesystem::path file_path_copy2{m_test_dir / "existing_file_2.txt"};
-   ASSERT_TRUE(std::filesystem::exists(file_path));
-   ASSERT_TRUE(std::filesystem::exists(file_path_copy1));
-   ASSERT_TRUE(std::filesystem::exists(file_path_copy2));
-
-   std::ifstream test_file(file_path);
-   std::string test_file_contents((std::istreambuf_iterator<char>(test_file)),
-                                  std::istreambuf_iterator<char>());
-   EXPECT_EQ(test_file_contents, "test string");
-
-   std::ifstream test_file_copy1(file_path_copy1);
-   std::string test_file_copy1_contents(
-       (std::istreambuf_iterator<char>(test_file_copy1)),
-       std::istreambuf_iterator<char>());
-   EXPECT_EQ(test_file_copy1_contents, "test string");
-
-   std::ifstream test_file_copy2(file_path_copy2);
-   std::string test_file_copy2_contents(
-       (std::istreambuf_iterator<char>(test_file_copy2)),
-       std::istreambuf_iterator<char>());
-   EXPECT_EQ(test_file_copy2_contents, "test string");
+   writeWrapper(m_test_dir / "existing_file.txt", m_test_string);
+   writeWrapper(m_test_dir / "existing_file.txt", m_test_string);
+   writeWrapper(m_test_dir / "existing_file.txt", m_test_string);
+   testSuccessfulWrite(m_test_dir / "existing_file.txt", m_test_string);
+   testSuccessfulWrite(m_test_dir / "existing_file_1.txt", m_test_string);
+   testSuccessfulWrite(m_test_dir / "existing_file_2.txt", m_test_string);
 }
 
 TEST_F(FileWritingTest, ThrowsOnNoWritePermissions) {
-   m_test_buffer << "test string";
+   m_test_buffer << m_test_string;
    std::filesystem::path no_write_dir{m_test_dir / "no_write_dir"};
    std::filesystem::create_directories(no_write_dir);
    std::filesystem::permissions(no_write_dir,
