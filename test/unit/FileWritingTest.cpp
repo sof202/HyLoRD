67e014125d2a7ab4d01edeafa17b409941ec01a6
sof202 <s.o.fletcher@exeter.ac.uk>
2025-05-16 13:23:35 +0100

    test(writing): Add basic test for file writing

---
+++ test/unit/FileWritingTest.cpp
@@ -0,0 +1,37 @@
+#include <gtest/gtest.h>
+
+#include <filesystem>
+#include <fstream>
+#include <iterator>
+#include <sstream>
+
+#include "io/writeMetrics.hpp"
+
+namespace Hylord {
+
+class FileWritingTest : public ::testing::Test {
+  public:
+   std::filesystem::path m_test_dir;
+   std::stringstream m_test_buffer;
+
+  protected:
+   void SetUp() override {
+      m_test_dir = std::filesystem::temp_directory_path() / "test";
+      std::filesystem::create_directories(m_test_dir);
+   }
+   void TearDown() override { std::filesystem::remove_all(m_test_dir); }
+};
+
+TEST_F(FileWritingTest, BasicFunctionality) {
+   m_test_buffer << "test string";
+   std::filesystem::path file_path{m_test_dir / "basic_writing.txt"};
+   IO::writeToFile(m_test_buffer, file_path);
+
+   ASSERT_TRUE(std::filesystem::exists(file_path));
+
+   std::ifstream test_file(file_path);
+   std::string test_file_contents((std::istreambuf_iterator<char>(test_file)),
+                                  std::istreambuf_iterator<char>());
+   EXPECT_EQ(test_file_contents, "test string");
+}
+}  // namespace Hylord
