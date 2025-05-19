077b3e9642150372bbe9882efb0d8106ce5a2d4c
sof202 <s.o.fletcher@exeter.ac.uk>
2025-05-19 10:40:32 +0100

    docs(google): add google verification for web crawlers

--- docs/doxyfile.cmake
+++ docs/doxyfile.cmake
@@ -157,7 +157,8 @@ HTML_EXTRA_STYLESHEET  = @AWESOME_CSS_DIR@/doxygen-awesome.css \
                          @AWESOME_CSS_DIR@/doxygen-awesome-sidebar-only.css \
                          ${PROJECT_SOURCE_DIR}/docs/hylord-doxygen-stylesheet.css
 HTML_EXTRA_FILES       = @AWESOME_CSS_DIR@/doxygen-awesome-darkmode-toggle.js \
-                         @AWESOME_CSS_DIR@/doxygen-awesome-paragraph-link.js
+                         @AWESOME_CSS_DIR@/doxygen-awesome-paragraph-link.js \
+                         ${PROJECT_SOURCE_DIR}/docs/google6830041316decdfa.html
 DISABLE_INDEX          = NO
 FULL_SIDEBAR           = NO
 GENERATE_TREEVIEW      = YES
