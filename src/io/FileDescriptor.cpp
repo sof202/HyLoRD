bb3a5d2a144b0e36ed226072be9feeddc770552b
sof202 <s.o.fletcher@exeter.ac.uk>
2025-05-27 13:12:42 +0100

    refactor: Use consistent exception types in FileDescriptor

--- src/io/FileDescriptor.cpp
+++ src/io/FileDescriptor.cpp
@@ -11,14 +11,13 @@ namespace Hylord::IO {
 void FileDescriptor::setup(const std::filesystem::path& file_path) {
    m_file_descriptor = open(file_path.c_str(), O_RDONLY);
    if (!valid()) {
-      throw std::system_error(
-          errno, std::system_category(), "Failed to open file");
+      throw FileReadException(file_path, errno, "Failed to open file");
    }
    if (fstat(m_file_descriptor, &m_file_info) == -1) {
       int fstat_error_number = errno;
       teardown();
-      throw std::system_error(
-          fstat_error_number, std::system_category(), "fstat failed for file");
+      throw FileReadException(
+          file_path, fstat_error_number, "fstat failed for file");
    }
    if (!S_ISREG(m_file_info.st_mode)) {
       teardown();
